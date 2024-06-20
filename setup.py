10d44c6ccd167fc45200e350e0221779b9b71bdf
wendihan <51539780+wendihan@users.noreply.github.com>
2024-06-20 10:52:21 +0800

    Chang package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='WH_Alpha191',
+    name='WHAlpha191',
     version='0.1',
     packages=find_packages(),
     description='GTJA Alpha 191',

04e7b0937da6f92c38b6f365cb40d2a563c28f27
wendihan <51539780+wendihan@users.noreply.github.com>
2024-06-20 10:40:36 +0800

    Create setup.py
    An initial trial of making this alpha 191 into a package.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='WH_Alpha191',
+    version='0.1',
+    packages=find_packages(),
+    description='GTJA Alpha 191',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='wh',
+    author_email='wendyhanmath@gmail.com',
+    url='https://github.com/wendihan/Alpha-101-GTJA-191',
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
+    classifiers=[
+        # Choose your license as you wish
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

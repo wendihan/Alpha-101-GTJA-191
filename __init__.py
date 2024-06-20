14393de49bda5b4968781c1633f6ac55fd81de72
wendihan <51539780+wendihan@users.noreply.github.com>
2024-06-20 11:00:50 +0800

    Create __init__.py
    An initial trial, haven't thought about it

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+
+# # You can define what gets imported when using 'from package import *'
+# __all__ = ['module1', 'module2']
+
+# # Importing modules from the package for convenience
+# from .module1 import Class1, function1
+# from .module2 import Class2, function2
